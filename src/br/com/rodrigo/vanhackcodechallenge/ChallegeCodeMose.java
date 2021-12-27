8fcd8bf75dc47e6204e69757194364e086b35270
Rodrigo Pereira <rodrigopereira.dev@gmail.com>
2021-12-27 18:28:20 -0300

    Versão refatorada

--- src/br/com/rodrigo/vanhackcodechallenge/ChallegeCodeMose.java
+++ src/br/com/rodrigo/vanhackcodechallenge/ChallegeCodeMose.java
@@ -21,25 +21,49 @@ public class ChallegeCodeMose {
     }
 
     private static void setPossibilities(String signals, ArrayList<String> possibilities, Map<String, String> morse) {
-        for (int i = 0; i < signals.length(); i++) {
-            for (var entry : morse.entrySet()) {
-                if (!possibilities.contains(entry.getKey())) {
-                    var tmpSignal = signals;
-                    if (tmpSignal.length() == entry.getValue().length()) {
-                        if (tmpSignal.charAt(i) == '?')
-                            tmpSignal = getcompatibleValue(signals, entry.getValue(), i);
 
-                        if (tmpSignal.equals(entry.getValue()))
-                            possibilities.add(entry.getKey());
-                    }
-                }
+        var countUnknownChars = getCountOfUnknownChars(signals.toCharArray());
+
+        // run via morse codes that exists
+        for (var entry : morse.entrySet()) {
+
+            if (signals.equals(entry.getValue())) {
+                possibilities.add(entry.getKey());
             }
+
+            // If the length of signals and morse code are equals, it means that may exist compatibility between them.
+            if (countUnknownChars > 0 && signals.length() == entry.getValue().length()) {
+                var tmpSignal = signals; // A temporary signals to receive morse code compatible
+                tmpSignal = getcompatibleValue(signals, entry.getValue(), tmpSignal.indexOf('?'));
+
+
+                if (tmpSignal.equals(entry.getValue()))
+                    possibilities.add(entry.getKey());
+
+            }
+
         }
+
     }
 
+    // This method retrieve the quantity of chars '?' that exists in the array
+    private static int getCountOfUnknownChars(char[] charArr) {
+        var countUnknownChars  = 0;
+
+        for (char c : charArr) {
+            if (c == '?')
+                countUnknownChars++;
+        }
+
+        return countUnknownChars;
+    }
+
+    // This method retrieve the morse code compatible with the signals. It will run while unknown chars exists.
     private static String getcompatibleValue(String signals, String morse, int i) {
+        // This will overwrite the unknown char for the equivalent char in the morse code. The index is used to do it.
         signals = signals.substring(0, i) + morse.charAt(i) + signals.substring(i + 1);
 
+        // If exists unknown chars yet, this method will be call recursively.
         if (signals.contains("?")) {
             signals = getcompatibleValue(signals, morse, signals.indexOf('?'));
         }
